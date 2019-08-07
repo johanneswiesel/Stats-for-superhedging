e29c5c475cc34991dc5c58304a57674255e7b961
johanneswiesel <52124949+johanneswiesel@users.noreply.github.com>
2019-08-07 11:47:36 +0200

    Update Figure2.py

--- Figure 2/Figure2.py
+++ Figure 2/Figure2.py
@@ -5,49 +5,42 @@
 """
 
 import numpy as np
-from Plugin_estimator import plugin_gurobi
 import matplotlib.pyplot as plt
-import scipy.stats as sct
-from scipy.optimize import minimize
+import pylab
+from Plugin_estimator import plugin_gurobi
 
-def g(x):
-    return np.maximum(x-2,0)
 
-def percentile(a,p):
-    return np.sort(a)[int(p*len(a))-1:]
-
-#Approximation of Wasserstein estimator
-def Wasserstein_approx(runs, N, F, g):
-    res = np.zeros((N,runs))
-    data = F(np.random.rand(N,runs))
-    for j in range(0, runs):
-        print(j)
-        for i in range(1, N):
-            cc= 1/np.power(i, 0.25)
-            def avar_cpt(x):
-                return np.mean(percentile(g(data[:i,j])-x[0]*(data[:i,j]-1), 1-cc))
-            res[i,j] = minimize(avar_cpt, x0=[0], method='BFGS').fun+ +2*cc
-            res[res<0] = 2
-    return(res)
-    
 if __name__ == '__main__':
-
-    # Runs and plots
+    # No of runs and samples
     runs = 10**3
-    N = 10**5
+    N = 10**4
     
     def g(x):
-        return(np.maximum(x-2,0))
-    #test log-normal
+        return np.array([int(i <= 0.5) for i in x])
+
+    #test exponential
+    la = 1
+    def F(x,la):
+        return -np.log(1-x)/la
     def F1(x):
-        return np.exp(sct.norm.ppf(x))
-    plugin_estimator = plugin_gurobi(runs, N, F1, g)
-    wasserstein_estimator = Wasserstein_approx(runs, N, F1, g)
+        return F(x,la)
+    
+    val = plugin_gurobi(runs, N, F1, g)
     
     plt.figure(figsize=(16.000, 8.000), dpi=100)
-    plt.loglog(range(0,N), plugin_estimator.mean(axis=1), label="Plugin")
-    plt.loglog(range(0,N), wasserstein_estimator.mean(axis=1), label="Wasserstein")
-    plt.loglog(range(0,N), np.repeat(1,N), label="True Value")
-    plt.legend()
-    plt.tight_layout()
+    plt.loglog(range(0,N),val.mean(axis=1), label="Plugin")
+    plt.loglog(range(0,N), val.mean(axis=1)+1/np.log(range(1,N+1)), label="Penalty")
+    plt.plot(range(0,N), np.repeat(1,N), label="True Value")
+    plt.legend(loc=4, prop={'size': 20})
+    import matplotlib.ticker as ticker
+    ax = plt.axes()
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.yaxis.set_major_locator(ticker.NullLocator())
+    ax.yaxis.set_minor_locator(plt.MaxNLocator(12))
+    ax.yaxis.set_minor_formatter(matplotlib.ticker.FormatStrFormatter("%.1f"))
+    
+    
+    pylab.savefig('hyb_exp.pdf')
+    plt.clf()
 
